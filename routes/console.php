64e65bc26c19137bff902335a6306a967e36f292
Guilledll <guilledll20@gmail.com>
2021-09-21 23:40:27 -0300

    artisan start

--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,10 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+  $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('start', function () {
+  $this->call('migrate');
+  $this->call('serve');
+});
